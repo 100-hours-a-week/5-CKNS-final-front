51024b494596e234fd7273a5f9c557c65952a83b
collie-jun <167491677+collie-jun@users.noreply.github.com>
2024-08-18 23:57:42 +0900

    Fix: bottom nav

--- travelday-fe/src/components/shared/bottomNav.js
+++ travelday-fe/src/components/shared/bottomNav.js
@@ -1,28 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useLocation } from 'react-router-dom';
 import homeIcon from '../../images/footer/home.png';
 import airplaneIcon from '../../images/footer/airplane.png';
 import scheduleIcon from '../../images/footer/schedule.png';
 import mapIcon from '../../images/footer/map.png';
 
 const BottomNav = () => {
+  const location = useLocation(); // 현재 경로를 가져옴
+
   return (
     <NavContainer>
-      <NavItem href="/">
+      <NavItem href="/" isActive={location.pathname === '/'}>
         <NavIcon src={homeIcon} alt="메인" />
-        <NavText>메인</NavText>
+        <NavText isActive={location.pathname === '/'}>메인</NavText>
       </NavItem>
-      <NavItem href="/search">
+      <NavItem href="/search" isActive={location.pathname === '/search'}>
         <NavIcon src={airplaneIcon} alt="예약" />
-        <NavText>예약</NavText>
+        <NavText isActive={location.pathname === '/search'}>예약</NavText>
       </NavItem>
-      <NavItem href="/schedule">
+      <NavItem href="/schedule" isActive={location.pathname === '/schedule'}>
         <NavIcon src={scheduleIcon} alt="일정" />
-        <NavText>일정</NavText>
+        <NavText isActive={location.pathname === '/schedule'}>일정</NavText>
       </NavItem>
-      <NavItem href="map">
+      <NavItem href="/map" isActive={location.pathname === '/map'}>
         <NavIcon src={mapIcon} alt="지도" />
-        <NavText>지도</NavText>
+        <NavText isActive={location.pathname === '/map'}>지도</NavText>
       </NavItem>
     </NavContainer>
   );
@@ -48,16 +51,22 @@ const NavItem = styled.a`
   flex-direction: column;
   align-items: center;
   text-decoration: none;
-  color: #000;
+  color: ${({ isActive }) => (isActive ? '#007bff' : '#000')}; 
+  transition: transform 0.2s, color 0.2s;
+
+  &:active {
+    transform: scale(1.1); 
+  }
 `;
 
 const NavIcon = styled.img`
   width: 23px;
   height: 23px;
   margin-bottom: 5px;
+  filter: ${({ isActive }) => (isActive ? 'invert(50%) sepia(100%) saturate(500%) hue-rotate(200deg)' : 'none')}; 
 `;
 
 const NavText = styled.span`
   font-size: 12px;
+  color: ${({ isActive }) => (isActive ? '#007bff' : '#000')}; /* 활성화된 아이템의 텍스트 색상 변경 */
 `;
-
