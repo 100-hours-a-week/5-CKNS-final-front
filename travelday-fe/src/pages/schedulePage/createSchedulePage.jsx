32bab81345292e2c93faf21d5e6eb98835298815
collie-jun <167491677+collie-jun@users.noreply.github.com>
2024-08-26 10:25:18 +0900

    Fix: 여행방 생성 로직

--- travelday-fe/src/pages/schedulePage/createSchedulePage.jsx
+++ travelday-fe/src/pages/schedulePage/createSchedulePage.jsx
@@ -14,12 +14,12 @@ const CreateSchedulePage = () => {
 
   const today = new Date().toISOString().split('T')[0];
 
-  // useEffect(() => {
-  //   const token = localStorage.getItem('accessToken');
-  //   if (!token) {
-  //     navigate('/login');
-  //   }
-  // }, [navigate]);
+  useEffect(() => {
+    const token = localStorage.getItem('accessToken');
+    if (!token) {
+      navigate('/login');
+    }
+  }, [navigate]);
 
   useEffect(() => {
     if (title && startDate && endDate) {
@@ -29,13 +29,13 @@ const CreateSchedulePage = () => {
     }
   }, [title, startDate, endDate]);
 
-  
-  const handleCreateSchedule =  () => {
+  const handleCreateSchedule = async () => {
     if (!isButtonEnabled) return;
 
-    const token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIzNjc2NjE1MzkyIiwiYXV0aCI6IlJPTEVfVVNFUiIsImV4cCI6MTcyNDYwMTQxOH0.m7-jJ6lUIA6ZfMN16S2xr72Fr-L8SMr30GvtjAkyMKo"
+    const token = localStorage.getItem('accessToken');
+
     try {
-      axios.post(
+      const response = await axios.post(
         'https://api.thetravelday.co.kr/api/rooms',
         {
           name: title,
@@ -49,40 +49,16 @@ const CreateSchedulePage = () => {
           },
           withCredentials: true 
         }
-      ).then(response =>{
-        console.log(response)
-      }
       );
 
-      // if (response.status === 201) { 
-      //   console.log('새로운 일정 생성:', response.data);
-      //   navigate('/schedule');
-      // }
+      if (response.status === 201) { 
+        console.log('새로운 일정 생성:', response.data);
+        navigate('/schedule');
+      }
     } catch (error) {
       console.error('일정 생성 중 오류 발생:', error);
     }
   };
-  
-  const api1 = async () => {
-    const token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIzNjc2NjE1MzkyIiwiYXV0aCI6IlJPTEVfVVNFUiIsImV4cCI6MTcyNDYwMTQxOH0.m7-jJ6lUIA6ZfMN16S2xr72Fr-L8SMr30GvtjAkyMKo"
-
-    const response = await axios.post(
-        `https://api.thetravelday.co.kr/api/rooms`,
-        {
-            name: "123",
-            startDate: "2024-09-01",
-            EndDate: "2024-09-02",
-        },
-        // {
-        //   headers: {
-        //     Authorization: `Bearer ${token}`,
-        //     'Content-Type': 'application/json',
-        //   },
-        //   withCredentials: true 
-        // }
-    )
-console.log(response)
-}
 
   return (
     <Container>
@@ -117,7 +93,7 @@ console.log(response)
           />
         </InputField>
         <CreateButton 
-          onClick={api1} 
+          onClick={handleCreateSchedule} 
           disabled={!isButtonEnabled} 
           enabled={isButtonEnabled}
         >
