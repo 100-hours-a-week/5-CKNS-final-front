ddbdb3bb25dd1926ebdbc2485d0b2a141137bc06
collie-jun <167491677+collie-jun@users.noreply.github.com>
2024-08-27 13:56:54 +0900

    Fix: 일정 삭제 로직 추가

--- travelday-fe/src/components/schedulePage/scheduleList.js
+++ travelday-fe/src/components/schedulePage/scheduleList.js
@@ -42,10 +42,6 @@ const ScheduleList = ({ schedules, onItemClick, onDeleteClick }) => {
   const confirmDelete = async () => {
     if (selectedScheduleId) {
       try {
-        // 삭제 요청 전에 로그 추가
-        console.log('삭제하려는 일정 ID:', selectedScheduleId);
-        
-        // 수정된 엔드포인트와 헤더 추가
         const response = await axios.delete(
           `https://api.thetravelday.co.kr/api/rooms/${selectedScheduleId}`,
           {
@@ -55,17 +51,19 @@ const ScheduleList = ({ schedules, onItemClick, onDeleteClick }) => {
             },
           }
         );
-
-        // 삭제 후 상태 업데이트
-        setSortedSchedules(sortedSchedules.filter(schedule => schedule.id !== selectedScheduleId));
-        onDeleteClick(selectedScheduleId);
-
-        setIsModalOpen(false);
-        window.alert('삭제되었습니다!');
+  
+        // 상태 코드가 200인 경우에만 후속 작업 실행
+        if (response.status === 200) {
+          setSortedSchedules(sortedSchedules.filter(schedule => schedule.id !== selectedScheduleId));
+          onDeleteClick(selectedScheduleId);
+          setIsModalOpen(false);
+          window.alert('삭제되었습니다!');
+        } else {
+          console.error(`Unexpected status code: ${response.status}`);
+        }
       } catch (error) {
-        // 에러 로그 추가
         console.error("일정 삭제 중 오류 발생:", error);
-        // 서버 응답에 대한 에러 로그 추가
+  
         if (error.response) {
           console.error("서버에서 반환된 에러 응답 데이터:", error.response.data);
           console.error("서버에서 반환된 에러 상태 코드:", error.response.status);
@@ -75,6 +73,7 @@ const ScheduleList = ({ schedules, onItemClick, onDeleteClick }) => {
       console.error("selectedScheduleId가 설정되지 않았습니다.");
     }
   };
+  
 
 
   const closeModal = () => {
