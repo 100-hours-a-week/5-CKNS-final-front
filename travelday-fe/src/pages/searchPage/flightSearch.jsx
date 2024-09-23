3802b61b96f68ca07cb9dde36780dc95b59ad548
collie-jun <167491677+collie-jun@users.noreply.github.com>
2024-09-23 21:40:23 +0900

    Feat: 검색페이지 로직 추가

--- travelday-fe/src/pages/searchPage/flightSearch.jsx
+++ travelday-fe/src/pages/searchPage/flightSearch.jsx
@@ -1,255 +1,3 @@
-// import React, { useState, useEffect } from 'react';
-// import { useNavigate } from 'react-router-dom'; 
-// import styled, { keyframes } from 'styled-components';
-// import TripTypeSelector from '../../components/searchPage/tripType';
-// import AreaPopup from '../../components/shared/areaPopup';
-// import DateRangePopup from '../../components/shared/datePopup';
-// import GuestSelectorPopup from '../../components/shared/guestPopup';
-// import FlightList from '../../components/searchPage/flightList';
-// import useFlightStore from '../../store/useFlightStore'; 
-// import { getFlights } from '../../utils/flightSearch'; 
-
-// // 이미지 파일 import
-// import img1 from '../../images/search/1.png';
-// import img2 from '../../images/search/2.png';
-// import img3 from '../../images/search/3.png';
-
-// const FlightSearch = () => {
-//   const [tripType, setTripType] = useState('round-trip');
-//   const [isPopupOpen, setIsPopupOpen] = useState(false);
-//   const [isDatePopupOpen, setIsDatePopupOpen] = useState(false);
-//   const [isGuestPopupOpen, setIsGuestPopupOpen] = useState(false);  
-//   const [searchType, setSearchType] = useState('');
-//   const [searchInput, setSearchInput] = useState('');
-//   const [filteredResults, setFilteredResults] = useState([]);
-//   const [flights, setFlights] = useState([]); 
-
-//   const {
-//     departure,
-//     arrival,
-//     dates,
-//     adults,
-//     children,
-//     setDeparture,
-//     setArrival,
-//     setDates,
-//     setAdults,
-//     setChildren,
-//   } = useFlightStore();
-
-//   const navigate = useNavigate();
-
-//   const departureLocations = ['서울', '부산', '인천', '대구']; // 수정된 부분
-//   const arrivalLocations = ['뉴욕', '파리', '도쿄', '런던']; // 수정된 부분
-
-//   useEffect(() => {
-//     // console.log("Selected Departure:", departure);
-//     // console.log("Selected Arrival:", arrival);
-//     // console.log("Selected Dates:", dates);
-//     // console.log("Adults:", adults);
-//     // console.log("Children:", children);
-//   }, [departure, arrival, dates, adults, children]);
-
-//   const fetchFlights = async () => {
-//     try {
-//       const params = {
-//         departure,
-//         arrival,
-//         startDate: dates.startDate.toISOString().split('T')[0], 
-//         endDate: dates.endDate.toISOString().split('T')[0],
-//         adults,
-//         children,
-//       };
-//       const data = await getFlights(params);
-//       setFlights(data); // 가져온 데이터를 상태에 저장
-//     } catch (error) {
-//       console.error('Failed to fetch flights:', error);
-//     }
-//   };
-
-//   const handlePopupClose = () => {
-//     setIsPopupOpen(false);
-//     setSearchInput('');
-//     setFilteredResults([]);
-//   };
-
-//   const handleButtonClick = (type) => {
-//     setSearchType(type);
-//     setIsPopupOpen(true);
-//   };
-
-//   const handleSearchInputChange = (event) => {
-//     setSearchInput(event.target.value);
-//   };
-
-//   const handleSearch = () => {
-//     const dataToFilter = searchType === 'departure' ? departureLocations : arrivalLocations;
-//     const results = dataToFilter.filter(location => location.includes(searchInput));
-//     setFilteredResults(results.length > 0 ? results : ['검색결과가 없습니다.']);
-//   };
-
-//   const handleKeyDown = (event) => {
-//     if (event.key === 'Enter') {
-//       handleSearch();
-//     }
-//   };
-
-  // const handleResultClick = (result) => {
-  //   if (searchType === 'departure') {
-  //     setDeparture(result);
-  //     handlePopupClose();
-  //     setTimeout(() => {
-  //       handleButtonClick('arrival');
-  //     }, 300);
-  //   } else {
-  //     setArrival(result); 
-  //     handlePopupClose();
-  //     setTimeout(() => {
-  //       setIsDatePopupOpen(true);
-  //     }, 300);
-  //   }
-  // };
-
-//   const handleDateSelect = (range) => {
-//     setDates(range);
-//   };
-
-//   const handleSearchClick = () => {
-//     setIsDatePopupOpen(false);  // 날짜 선택 팝업 닫기
-//     setIsGuestPopupOpen(true);  // 인원 선택 팝업 열기
-//   };
-
-//   const handleGuestSelect = async (adults, children) => {
-//     setAdults(adults);
-//     setChildren(children);
-//     await fetchFlights(); // 인원 선택 후, API 호출
-//     navigate('/flight'); // 이후 /flight 페이지로 이동
-//   };
-
-//   const handleDatePopupClose = () => {
-//     setIsDatePopupOpen(false);
-//     setDates([]);
-//   };
-
-//   return (
-//     <Container>
-//       <TripTypeSelector tripType={tripType} setTripType={setTripType} />
-//       <AreaSearchingContainer>
-//         <ButtonContainer>
-//           <Button onClick={() => handleButtonClick('departure')}>출발지</Button>
-//           <SwitchIcon src={switchIcon} alt="Switch" />
-//           <Button onClick={() => handleButtonClick('arrival')}>도착지</Button>
-//         </ButtonContainer>
-//       </AreaSearchingContainer>
-//       <AnimatedPopup 
-//         isOpen={isPopupOpen} 
-//         onClose={handlePopupClose} 
-//         searchResults={filteredResults}
-//         onResultClick={handleResultClick} 
-//       >
-//         <input 
-//           type="text" 
-//           placeholder={searchType === 'departure' ? "출발지를 검색하세요." : "도착지를 검색하세요."} 
-//           value={searchInput}
-//           onChange={handleSearchInputChange} 
-//           onKeyDown={handleKeyDown} 
-//         />
-//       </AnimatedPopup>
-//       <DateRangePopup 
-//         isOpen={isDatePopupOpen} 
-//         onClose={handleDatePopupClose}
-//         onDateRangeChange={handleDateSelect}
-//         onSearchClick={handleSearchClick} 
-//         toggleLabel="가는날 - 오는날"
-//         buttonText="검색"
-//         dateRange={dates} 
-//       />
-//       {isGuestPopupOpen && (
-//         <GuestSelectorPopup 
-//           isOpen={isGuestPopupOpen}
-//           onClose={() => setIsGuestPopupOpen(false)}
-//           onGuestSelect={handleGuestSelect}
-//           adults={adults}
-//           children={children}
-//         />
-//       )}
-//       <FlightList flights={flights.length > 0 ? flights : [
-//         { image: img1, country: '미국', city: 'New York', schedule: '2024. 11. 16 - 11.18', price: '623,000원' },
-//         { image: img2, country: '프랑스', city: 'Paris', schedule: '2024. 9. 12 - 9.18', price: '1,092,000원' },
-//         { image: img3, country: '일본', city: 'Tokyo', schedule: '2024. 8. 23 - 8.30', price: '340,000원' },
-//       ]} />  
-//     </Container>
-//   );
-// };
-
-// export default FlightSearch;
-
-
-// const slideUp = keyframes`
-//   from {
-//     transform: translateY(100%);
-//     opacity: 0;
-//   }
-//   to {
-//     transform: translateY(0);
-//     opacity: 1);
-//   }
-// `;
-
-// const Container = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   align-items: center;
-// `;
-
-// const ButtonContainer = styled.div`
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-//   width: 327px;
-//   height: 64px;
-//   background-color: #fff;
-//   box-shadow: 0px 0px 10px rgba(149, 157, 177, 0.3);
-//   border-radius: 8px;
-//   margin-bottom: 10px;
-// `;
-
-// const Button = styled.button`
-//   padding: 8px 16px;
-//   margin: 0 20px;
-//   cursor: pointer;
-//   background-color: #fff;
-//   border: none;
-//   outline: none;
-// `;
-
-// const AreaSearchingContainer = styled.div`
-//   width: 390px;
-//   display: flex;
-//   align-items: center;
-//   background-color: #fff;
-//   justify-content: center;
-// `;
-
-// const AnimatedPopup = styled(AreaPopup)`
-//   animation: ${slideUp} 0.3s ease-out;
-//   position: fixed;
-//   bottom: 0;
-//   left: 0;
-//   right: 0;
-//   background-color: #fff;
-//   border-top-left-radius: 10px;
-//   border-top-right-radius: 10px;
-//   box-shadow: 0px -4px 10px rgba(0, 0, 0, 0.1);
-//   padding: 20px;
-//   z-index: 1000;
-//   overflow-y: hidden;
-
-//   ${(props) => !props.isOpen && `
-//     display: none;
-//   `}
-// `;
-
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom'; 
 import styled from 'styled-components';
@@ -259,7 +7,7 @@ import { getFlights } from '../../utils/flightSearch';
 import switchIcon from '../../images/switch.png';
 import AreaPopup from '../../components/shared/areaPopup';
 import TripTypeSelector from '../../components/searchPage/tripType';
-
+import axiosInstance from '../../utils/axiosInstance'; 
 import img1 from '../../images/search/1.png';
 import img2 from '../../images/search/2.png';
 import img3 from '../../images/search/3.png';
@@ -270,11 +18,7 @@ const FlightSearch = () => {
   const [flights, setFlights] = useState([]); 
 
   const {
-    departure,
-    setDeparture,
-    arrival,
-    setArrival,
-  } = useFlightStore();
+    departure,setDeparture,arrival,setArrival} = useFlightStore();
 
   const navigate = useNavigate();
 
@@ -286,26 +30,65 @@ const FlightSearch = () => {
     '뉴욕 라과디아(LGA)', '나고야 츄부(NGO)', '파리 샤를드골(CDG)', '호주 시드니(SYD)',
     '마드리드(MAD)', '런던 히드로(LHR)', '비엔나(VIE)', '프랑크푸르트(FRA)', '로마 피우미치노(FCO)'
   ];
-
   const fetchFlights = async () => {
     try {
-      const params = { departure, arrival };
-      const data = await getFlights(params);
-      setFlights(data); 
+      // localStorage에서 액세스 토큰 가져오기
+      const accessToken = localStorage.getItem('accessToken');
+      const exchangeRate = 1400; // 1 유로당 1400원
+  
+      // IATA 코드에 따른 이미지 및 국가 매핑
+      const destinationInfo = {
+        JFK: { city: '뉴욕', country: '미국', image: img1 },
+        LGA: { city: '뉴욕', country: '미국', image: img1 },
+        CDG: { city: '파리', country: '프랑스', image: img2 },
+        NRT: { city: '도쿄', country: '일본', image: img3 },
+        HND: { city: '도쿄', country: '일본', image: img3 }
+      };
+  
+      const response = await axiosInstance.get('/api/flights/lowestPrice/list', {
+        headers: { Authorization: `Bearer ${accessToken}` },
+        withCredentials: true, // 쿠키를 전송하기 위한 옵션
+      });
+  
+      const flightData = response.data.data;
+  
+      // 항공편 데이터 필터링 및 매핑
+      const mappedFlights = flightData
+        .map(flight => {
+          const arrivalIata = flight.itineraries[0].segments[0].arrival.iataCode;
+          const destination = destinationInfo[arrivalIata];
+  
+          if (!destination) return null; // 매칭되지 않으면 null 반환
+  
+          const priceInWon = Math.round(flight.price.total * exchangeRate);
+          return {
+            image: destination.image,
+            country: destination.country,
+            city: destination.city,
+            schedule: flight.lastTicketingDate || '2024. 11. 16 - 11.18',
+            price: `${priceInWon.toLocaleString()}원`
+          };
+        })
+        .filter(Boolean); // null 값 제거
+  
+      setFlights(mappedFlights); // 상태에 저장
+      console.log('필터링된 항공권 데이터:', mappedFlights);
     } catch (error) {
-      console.error('Failed to fetch flights:', error);
+      console.error('항공권 데이터를 가져오는데 실패:', error);
     }
   };
-
+  
+  
   const handlePopupClose = () => {
     setIsPopupOpen(false);
   };
 
-    useEffect(() => {
+  useEffect(() => {
     setDeparture('인천');
     setArrival('인천');
-  }, [setDeparture, setArrival]); 
-
+    fetchFlights(); // 여기에 fetchFlights 호출 추가
+  }, [setDeparture, setArrival]);
+  
 
   const handleTripTypeChange = (type) => {
     setTripType(type);
@@ -327,7 +110,7 @@ const FlightSearch = () => {
             <>
               <FixedDeparture>{departure}</FixedDeparture> 
               <SwitchIcon src={switchIcon} alt="Switch" />
-              <Button onClick={() => setIsPopupOpen(true)}>도착지 검색</Button> 
+              <Button onClick={() => setIsPopupOpen(true)}>도착지 선택</Button> 
             </>
           ) : (
             <>
