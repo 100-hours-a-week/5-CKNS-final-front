90bbf6a238fcbbc3e790e741c7dc97fb41a22e9a
HidenLee <hyundo1995@gmail.com>
2024-09-25 22:42:43 +0900

    test: check registration of service worker

--- travelday-fe/public/firebase-messaging-sw.js
+++ travelday-fe/public/firebase-messaging-sw.js
@@ -1,31 +1,16 @@
-self.addEventListener("install", function (e) {
-    console.log("fcm sw install..");
-    self.skipWaiting();
-});
+import { getMessaging } from "firebase/messaging/sw";
+import { onBackgroundMessage } from "firebase/messaging/sw";
 
-self.addEventListener("activate", function (e) {
-    console.log("fcm sw activate..");
-});
-
-self.addEventListener("push", function (e) {
-    console.log("push: ", e.data.json());
-    if (!e.data.json()) return;
-
-    const resultData = e.data.json().notification;
-    const notificationTitle = resultData.title;
+const messaging = getMessaging();
+onBackgroundMessage(messaging, (payload) => {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    // Customize notification here
+    const notificationTitle = 'Background Message Title';
     const notificationOptions = {
-        body: resultData.body,
-        icon: resultData.image,
-
+        body: 'Background Message body.',
+        icon: '/firebase-logo.png'
     };
-    console.log("push: ", { resultData, notificationTitle, notificationOptions });
 
-    self.registration.showNotification(notificationTitle, notificationOptions);
-});
-
-self.addEventListener("notificationclick", function (event) {
-    console.log("notification click");
-    // const url = "/";
-    event.notification.close();
-    // event.waitUntil(clients.openWindow(url));
+    self.registration.showNotification(notificationTitle,
+        notificationOptions);
 });
--- travelday-fe/src/firebase-messaging-sw.js
+++ travelday-fe/src/firebase-messaging-sw.js
@@ -12,71 +12,72 @@ const firebaseConfig = {
     measurementId: "G-2R5M42P16Z"
 };
 
-// // Firebase 초기화
-// const app = initializeApp(firebaseConfig);
-//
-// // // FCM 초기화
-// const messaging = getMessaging(app);
-// //
-// export const requestForToken = (setTokenFound) => {
-//   return getToken(messaging, { vapidKey: "BIs8qF7l2tBm1Ygtf7g8_xdmAHbAf15yQ9bx-UAEYuPmOPDsO2P8cAO2ntlkyrQ40r5wZ6-fXm7BqbXAR7PBCXk" })
-//     .then((currentToken) => {
-//       if (currentToken) {
-//         // FCM 토큰을 백엔드로 전송하는 함수 호출
-//         sendTokenToServer(currentToken).then(response =>{console.log(response);});
-//         setTokenFound && setTokenFound(true); // setTokenFound가 전달된 경우에만 호출
-//       } else {
-//         setTokenFound && setTokenFound(false);
-//       }
-//     })
-//     .catch((err) => {
-//       console.error("An error occurred while retrieving token. ", err);
-//       setTokenFound && setTokenFound(false);
-//     });
-// };
-//
-// const sendTokenToServer = async (token) => {
-//     try {
-//     const response = await axiosInstance.post('/api/fcm',
-//       { token }, // 요청 바디
-//       {
-//         headers: {
-//           Authorization: `Bearer ${token}`
-//         }
-//       }
-//     );
-//
-//     console.log("서버로 토큰 전달 완료:", response.data);
-//   } catch (error) {
-//     console.error("토큰 전달시 에러 발생:", error);
-//   }
-// };
-
+// Firebase 초기화
 const app = initializeApp(firebaseConfig);
+
+// // FCM 초기화
 const messaging = getMessaging(app);
-
-async function requestPermission() {
-    console.log("권한 요청 중...");
-
-    const permission = await Notification.requestPermission();
-    if (permission === "denied") {
-        console.log("알림 권한 허용 안됨");
-        return;
-    }
-
-    console.log("알림 권한이 허용됨");
-
-    const token = await getToken(messaging, {
-        vapidKey: process.env.REACT_APP_VAPID_KEY,
+//
+export const requestForToken = (setTokenFound) => {
+  return getToken(messaging, { vapidKey: "BIs8qF7l2tBm1Ygtf7g8_xdmAHbAf15yQ9bx-UAEYuPmOPDsO2P8cAO2ntlkyrQ40r5wZ6-fXm7BqbXAR7PBCXk" })
+    .then((currentToken) => {
+      if (currentToken) {
+        console.log(currentToken);
+        // FCM 토큰을 백엔드로 전송하는 함수 호출
+        sendTokenToServer(currentToken).then(response =>{console.log(response);});
+        setTokenFound && setTokenFound(true); // setTokenFound가 전달된 경우에만 호출
+      } else {
+        setTokenFound && setTokenFound(false);
+      }
+    })
+    .catch((err) => {
+      console.error("An error occurred while retrieving token. ", err);
+      setTokenFound && setTokenFound(false);
     });
+};
 
-    if (token) console.log("token: ", token);
-    else console.log("Can not get Token");
+const sendTokenToServer = async (token) => {
+    try {
+    const response = await axiosInstance.post('/api/fcm',
+      { token }, // 요청 바디
+      {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }
+    );
 
-    onMessage(messaging, (payload) => {
-        console.log("메시지가 도착했습니다.", payload);
-        // ...
-    });
-}
+    console.log("서버로 토큰 전달 완료:", response.data);
+  } catch (error) {
+    console.error("토큰 전달시 에러 발생:", error);
+  }
+};
 
-requestPermission();
+// const app = initializeApp(firebaseConfig);
+// const messaging = getMessaging(app);
+//
+// async function requestPermission() {
+//     console.log("권한 요청 중...");
+//
+//     const permission = await Notification.requestPermission();
+//     if (permission === "denied") {
+//         console.log("알림 권한 허용 안됨");
+//         return;
+//     }
+//
+//     console.log("알림 권한이 허용됨");
+//
+//     const token = await getToken(messaging, {
+//         vapidKey: process.env.REACT_APP_VAPID_KEY,
+//     });
+//
+//     if (token) console.log("token: ", token);
+//     else console.log("Can not get Token");
+//
+//     onMessage(messaging, (payload) => {
+//         console.log("메시지가 도착했습니다.", payload);
+//         // ...
+//     });
+// }
+//
+// requestPermission();
